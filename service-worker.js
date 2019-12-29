bed68ea758902debc9765ce736158611eb07c97c
ash-uncover <ash.uncover@gmail.com>
2019-12-29 13:53:04 +0100

    removed bootstrap

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f4ab3095b626acf6da139842a1578aa.js"
+  "/precache-manifest.712aa161a04cdc4d0c1c7b025089ec5d.js"
 );
 
 self.addEventListener('message', (event) => {
